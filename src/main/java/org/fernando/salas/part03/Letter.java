e466e147f167edd8c528ab951aa18210181d98db
Fernando Salas <luisfernandosalasg@gmail.com>
2024-07-13 00:36:02 -0500

    Letter

---
+++ src/main/java/org/fernando/salas/part03/Letter.java
@@ -0,0 +1,19 @@
+package org.fernando.salas.part03;
+
+public class Letter {
+    public static String addHeader(String text) {
+        return "From Raoul, Mario and Alan: " + text;
+    }
+
+    public static String addFooter(String text) {
+        return text + " Kind regards";
+    }
+
+    public static String checkSpelling(String text) {
+        return text.replaceAll("labda", "lambda");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
