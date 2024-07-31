b76d6c88c92f9ab9dd3520ad6457be527108d2fa
Fernando Salas <luisfernandosalasg@gmail.com>
2024-07-31 01:19:57 -0500

    Using Optional

---
+++ src/main/java/org/fernando/salas/part11/optional/Insurance.java
@@ -0,0 +1,9 @@
+package org.fernando.salas.part11.optional;
+
+public class Insurance {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/org/fernando/salas/part11/optional/Person.java
@@ -0,0 +1,11 @@
+package org.fernando.salas.part11.optional;
+
+import java.util.Optional;
+
+public class Person {
+    private Optional<Car> car;
+
+    public Optional<Car> getCar() {
+        return car;
+    }
+}
